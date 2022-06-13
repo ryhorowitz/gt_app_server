bd8d2cff865181cd540c422df5b4e816f688a1c0
ryhorowitz <ryan.w.horowitz@gmail.com>
2022-06-13 15:43:09 -0400

    router '/search' set up

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const port = 1234
+const search = require('./routes/search');
+
+app.use('/search', search);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
