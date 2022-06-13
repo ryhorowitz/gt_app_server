a380ccd5fd37093c91b8102b86680eede2fa97dc
ryhorowitz <ryan.w.horowitz@gmail.com>
2022-06-13 16:26:06 -0400

    trying to link this backend to the client EOD

--- routes/search.js
+++ routes/search.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req,res) => {
-  res.send('Hello from search router!')
+  res.json({data: 'Hello from search router!'})
 })
 
 module.exports = router;
